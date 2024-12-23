1c7d77337ce47e872472adb70465395bd66eedf3
Yuan Shih <85390677+Yuan0100@users.noreply.github.com>
2024-12-23 22:29:14 +0800

    chores: update site and image url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://uanuanshi.github.io',
+  site: 'https://yuan0100.github.io/glsl-113-1/',
   base: '/glsl-113-1',
   // integrations: [mdx(), sitemap(), react()],
   vite: {
