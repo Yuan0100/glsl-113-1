37838fb848afd8197d01ee960363b4b0254ce031
Yuan Shih <85390677+Yuan0100@users.noreply.github.com>
2024-12-23 10:33:26 +0800

    feat: add Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://uanuanshi.github.io',
+  base: '/glsl-113-1',
+  // integrations: [mdx(), sitemap(), react()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler' // or "modern"
+        }
+      }
+    }
+  }
+});
